8926891f1af6ed9daf8439bf9df04c5d5fde845a
Bill <bfritz@uw.edu>
2023-03-10 14:57:22 -0800

    10/03/2023_14:57:22 Working in: /tmp/gns3/gns3-mgannon Committed by: bfritz

---
+++ rhel8-install.sh
@@ -0,0 +1,93 @@
+#!/usr/bin/env bash
+
+#--------------------------------------------------------------------------------------------------------------------------------
+#  *                              Rhel8Install - GNS3
+#    
+#    Author: Bill Fritz
+#    Description: Provision GNS3 VM with RHEL 8 and GNS3 (This script assumes the desktop environment is already installed)
+#    Last Modified: 2023-03-10
+#    
+#--------------------------------------------------------------------------------------------------------------------------------
+
+
+function preflight(){
+    # Check if RHEL 8
+    if [ "$(lsb_release -rs)" != "8.3" ]
+    then echo "This script is for RHEL 8"
+    exit
+    fi
+    # Fail if not root
+    if [ "$EUID" -ne 0 ]
+    then echo "Please run as root"
+    exit
+    fi
+}
+
+function baseconfig(){
+    # Disable SELinux
+    setenforce 0
+    sed -i 's/^SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
+    # Install base configs
+    dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+    subscription-manager repos --enable codeready-builder-for-rhel-8-x86_64-rpms
+    dnf install dnf-plugins-core -y
+    dnf config-manager --set-enabled powertools
+    # Dev tools
+    dnf -y install python3-devel elfutils-libelf-devel libpcap-devel python3-pyqt5-sip python3-qt5 xterm cmake
+    dnf -y groupinstall "Development Tools"
+    python3 -m pip install --upgrade pip
+    # Install Docker-ce
+    dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+    dnf install -y docker-ce
+    # Enable and start Docker
+    systemctl enable docker
+    systemctl start docker
+    # Add to docker group
+    usermod -a -G docker "$(whoami)"
+}
+
+function gns3build(){
+# GNS3 Src
+mkdir ~/gns3
+cd ~/gns3 || exit
+git clone https://github.com/GNS3/gns3-server.git
+git clone https://github.com/GNS3/gns3-gui.git
+git clone https://github.com/GNS3/vpcs.git
+git clone https://github.com/GNS3/dynamips.git
+git clone https://github.com/GNS3/ubridge.git
+
+cd ~/gns3/gns3-server/ || exit
+pip3 install -r requirements.txt
+python3 setup.py install
+
+cd ~/gns3/gns3-gui/ || exit
+pip3 install -r requirements.txt
+python3 setup.py install
+cp resources/linux/applications/gns3.desktop /usr/share/applications/
+cp -R resources/linux/icons/hicolor/ /usr/share/icons/
+
+cd ~/gns3/vpcs/src || exit
+./mk.sh
+cp vpcs /usr/local/bin/vpcs
+
+cd ~/gns3/dynamips/ || exit
+mkdir build 
+cd build/ || exit
+cmake ..
+make
+make install
+
+cd ~/gns3/ubridge || exit
+make
+make install
+
+}
+
+function main(){
+    preflight
+    baseconfig
+    gns3build
+    echo "Server installed. Type 'gns3' to start the GUI"
+}
+
+main
---
+++ ubuntu2004-install.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+#--------------------------------------------------------------------------------------------------------------------------------
+#  *                              Ubuntu2004Install
+#    
+#    Author: Bill Fritz
+#    Description: GNS3 Ubuntu 20.04 Install (This script assumes the desktop environment is already installed)
+#    Last Modified: 2023-03-10
+#    
+#--------------------------------------------------------------------------------------------------------------------------------
+
+function preflight(){
+  # Check if ubuntu 20.04
+  if [ "$(lsb_release -rs)" != "20.04" ]
+    then echo "This script is for Ubuntu 20.04"
+    exit
+  fi
+  # Fail if not root
+  if [ "$EUID" -ne 0 ]
+    then echo "Please run as root"
+    exit
+  fi
+}
+
+function baseconfig(){
+  # Install base configs
+  apt update
+  apt upgrade
+  add-apt-repository ppa:gns3/ppa
+}
+
+function gns3install(){
+  # Install GNS3 (Answer yes, yes)
+  sudo apt-get update
+  sudo apt-get install gns3-server gns3-gui
+
+  # IOU Support
+  dpkg --add-architecture i386
+  apt update
+  apt install gns3-iou
+
+  # Install Docker-ce
+  apt remove docker docker-engine docker.io 2>/dev/null
+  apt -y install lsb-release gnupg apt-transport-https ca-certificates curl software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/docker.gpg
+  add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  apt update
+  apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+  usermod -aG docker "$(whoami)"
+  newgrp docker
+
+  # Virt Support
+  usermod -aG ubridge libvirt kvm wireshark "$USER"
+}
+
+function main(){
+  preflight
+  baseconfig
+  gns3install
+  echo "Server installed. Type 'gns3' to start the GUI"
+}
